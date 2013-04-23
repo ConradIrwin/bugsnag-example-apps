037ae43521cbadc135a6b3dd15823077f9a46f6b
James Smith <james@loopj.com>
2013-04-22 20:34:38 -0700

    Update rails2 example app

--- rails2/app/controllers/application_controller.rb
+++ rails2/app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  before_bugsnag_notify :awesome
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  protected
+  def awesome(notification)
+    puts "\n\n\n\nJAMES AWESOME\n\n\n\n"
+  end
 end
--- rails2/config/initializers/bugsnag.rb
+++ rails2/config/initializers/bugsnag.rb
@@ -1,3 +1,4 @@
 Bugsnag.configure do |config|
   config.api_key = "908e5bd2815f286677e0d9bc92f6e1e1"
+  config.logger = Logger.new(STDOUT)
 end
