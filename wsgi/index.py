5a2fe918e726b68be5bdcaded2c4056e4e2d23a4
Conrad Irwin <conrad.irwin@gmail.com>
2014-04-29 13:34:57 -0700

    Fix wsgi

--- wsgi/index.py
+++ wsgi/index.py
@@ -11,20 +11,21 @@ def application(environ, start_response):
     if environ.get('PATH_INFO', '') == "/":
         d = parse_qs(environ['QUERY_STRING'])
 
+        start_response('200 OK', [('Content-Type', 'text/html')])
         raise Exception("Something broke")
 
-        start_response('200 OK', [('Content-Type', 'text/html')])
         return ["Some output here"]
     else:
         start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
         return ['Not Found']
 
 # Configure bugsnag
-bugsnag.configure(api_key="dcc345d219ef5107c6ce8aca68a40af2")
+bugsnag.configure(api_key="066f5ad3590596f9aa8d601ea89af845")
 
 # Add bugsnag wsgi middleware to app
 application = BugsnagMiddleware(application)
 
 # Start a server
+print("listening on :8051")
 httpd = make_server('localhost', 8051, application)
 httpd.serve_forever()
